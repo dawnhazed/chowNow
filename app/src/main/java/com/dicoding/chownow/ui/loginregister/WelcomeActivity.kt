1a62aeba11a45e56c1e745d2fe5be81ea5be5cf3
RakaiTambora <akbar.khaerullah.9@gmail.com>
2024-05-24 01:01:09 +0700

    changing assets png to svg vector and still finding why xml in not displaying

--- app/src/main/java/com/dicoding/chownow/ui/loginregister/WelcomeActivity.kt
+++ app/src/main/java/com/dicoding/chownow/ui/loginregister/WelcomeActivity.kt
@@ -1,47 +1,54 @@
 package com.dicoding.chownow.ui.loginregister
 
-import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.WindowInsets
 import android.view.WindowManager
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.dicoding.chownow.R
 import com.dicoding.chownow.databinding.ActivityWelcomeBinding
 
 class WelcomeActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityWelcomeBinding
+
+    private var binding: ActivityWelcomeBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityWelcomeBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        Log.d("WelcomeActivity", "Inflating layout")
 
-        setupView()
-        setupAction()
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+        Log.d("WelcomeActivity", "Layout inflated")
+
+        //setupView()
+        Log.d("setupView", "View settled")
+
+        //setupAction()
+        Log.d("setupAction", "View be Clicked")
     }
 
     private fun setupView() {
         @Suppress("DEPRECATION")
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             window.insetsController?.hide(WindowInsets.Type.statusBars())
+            Log.d("isAndroidRedVelvetOrLater", "Yap, android ini diatas atau sama dengan RedVelvet")
         } else {
             window.setFlags(
                 WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
             )
+            Log.d("isAndroidRedVelvetOrLater", "Tidak, android ini dibawah RedVelvet")
         }
         supportActionBar?.hide()
+        Log.d("ActionBar", "ActionBar disembunyikan")
     }
 
     private fun setupAction(){
-        binding.loginButton.setOnClickListener {
+        binding?.loginButton?.setOnClickListener {
             // startActivity(Intent(this, LoginActivity::class.java))
         }
 
-        binding.signupButton.setOnClickListener {
+        binding?.signupButton?.setOnClickListener {
             // startActivity(Intent(this, SignupActivity::class.java))
         }
     }

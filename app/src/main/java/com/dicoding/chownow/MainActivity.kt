291cb5c40ab047f518b42629b8096611f5098935
RakaiTambora <akbar.khaerullah.9@gmail.com>
2024-05-23 14:40:27 +0700

    tes

--- app/src/main/java/com/dicoding/chownow/MainActivity.kt
+++ app/src/main/java/com/dicoding/chownow/MainActivity.kt
@@ -17,5 +17,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        // contoh test aja
     }
 }

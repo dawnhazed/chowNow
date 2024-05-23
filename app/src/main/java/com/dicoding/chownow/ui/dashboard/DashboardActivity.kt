3eb265891cf13a3eccf078b89cafb9c52e161ab7
shafa <shafadoesthings@gmail.com>
2024-05-23 14:58:04 +0700

    Package Commit

---
+++ app/src/main/java/com/dicoding/chownow/ui/dashboard/DashboardActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.chownow.ui.dashboard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+
+class DashboardActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+    }
+}

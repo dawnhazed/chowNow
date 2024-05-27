fbe1776c9d3135b58f9618a7b19dfcce639ee100
RakaiTambora <akbar.khaerullah.9@gmail.com>
2024-05-27 18:14:51 +0700

    adding FoodItemAdapter for ConfirmOrderActivity, minor changes in layout, add item food and the data class

---
+++ app/src/main/java/com/dicoding/chownow/data/model/Food.kt
@@ -0,0 +1,3 @@
+package com.dicoding.chownow.data.model
+
+data class FoodItem(val name: String, val price: Int, val quantity: Int)
--- app/src/main/java/com/dicoding/chownow/ui/dashboard/resto/confirm/ConfirmOrderActivity.kt
+++ app/src/main/java/com/dicoding/chownow/ui/dashboard/resto/confirm/ConfirmOrderActivity.kt
@@ -5,17 +5,42 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.dicoding.chownow.R
+import com.dicoding.chownow.data.model.FoodItem
+import com.dicoding.chownow.databinding.ActivityConfirmOrderBinding
+import com.dicoding.chownow.ui.dashboard.resto.FoodItemAdapter
 
 class ConfirmOrderActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityConfirmOrderBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_confirm_order)
+        binding = ActivityConfirmOrderBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val foodItems = listOf(
+            FoodItem("Nama Makanan A", 25000, 1),
+            FoodItem("Nama Makanan B", 25000, 1),
+            FoodItem("Nama Makanan C", 25000, 1),
+            FoodItem("Nama Makanan D", 25000, 1),
+            FoodItem("Nama Makanan E", 25000, 1),
+            FoodItem("Nama Makanan F", 25000, 1),
+            FoodItem("Nama Makanan G", 25000, 1),
+            FoodItem("Nama Makanan H", 25000, 1)
+        )
+
+        val adapter = FoodItemAdapter(foodItems)
+        binding.rvFoodItems.adapter = adapter
+        binding.rvFoodItems.layoutManager = LinearLayoutManager(this)
+
     }
 }

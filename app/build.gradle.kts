e575f3f028251079f554ad32c44285863f48778d
RakaiTambora <akbar.khaerullah.9@gmail.com>
2024-05-27 12:20:31 +0700

    confirm order wip, resto adapter change, home fragment change, adding location activity, model resto

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,8 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.activity.ktx)
     implementation(libs.androidx.fragment.ktx.v171)
     implementation(libs.material)
@@ -63,9 +65,10 @@ dependencies {
     implementation(libs.androidx.fragment.ktx)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.activity)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation(libs.androidx.recyclerview)
 }
---
+++ app/src/main/java/com/dicoding/chownow/data/model/Resto.kt
@@ -0,0 +1,8 @@
+package com.dicoding.chownow.data.model
+
+data class Resto(
+    val imgResto: Int,
+    val namaResto: String,
+//    val jarak: Int,
+    val rating: Float
+)
